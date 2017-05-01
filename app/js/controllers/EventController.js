a95bab2359502c750acf98574c9ce8c37f950457
bvasic <vasicbie@gmail.com>
2017-05-01 19:33:52 +0100

    Updated json and directives
    sessions and ng-repeat

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -11,7 +11,30 @@ eventsApp.controller('EventController',
 				city: 'Mountain View',
 				province: 'CA'
 			},
-			imageUrl: '/img/angularjs-logo.png'
+			imageUrl: '/img/angularjs-logo.png',
+			sessions: [
+				{
+					name: 'Directives Masterclass',
+					creatorName: 'Bob Smith',
+					duration: '1 hr',
+					level: 'Advanced',
+					abstract: 'In this session you will learn the ins and outs of directives'
+				},
+				{
+					name: 'Scopes for fun and profit',
+					creatorName: 'Bojan Vasic',
+					duration: '45min',
+					level: 'Advanced',
+					abstract: 'Angular Fundamentals'
+				},
+				{
+					name: 'Well Behaved Controllers',
+					creatorName: 'John Wayne',
+					duration: '2 hr',
+					level: 'Medium',
+					abstract: 'This session will explain directives'
+				}
+			]
 		}
 	}
 
